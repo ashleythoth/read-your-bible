68c7a0d742ef9be1733254e80c243603406b62df
ashleythoth <100374976+ashleythoth@users.noreply.github.com>
2024-06-10 17:39:28 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
 $srv = "localhost";
-$db = "readyourbible";
-$usr = "readyourbible";
-$pass = "@hKU!I_)iyJ2)MdJ";
+$db = "dbname";
+$usr = "dbuser";
+$pass = "dbpass";
 
 $conn = new mysqli($srv, $usr, $pass, $db);
 // Check connection
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
 }
 
 // recaptcha v3
-$scrt = "6LeP1XgnAAAAAC87sIVo8flAf3AyzyWEogBKfhR3";
-$ky = "6LeP1XgnAAAAALyYPJqUW_WnoOhNjEOZwlwzKF9G";
+$ky = "your site key";
+$scrt = "your secret key";
 
 ?>
